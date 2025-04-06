85c0ddb2088f09866222735be5a2daa295a3228e
krishatuladhar <krishatuladhar@gmail.com>
2025-04-06 17:15:07 +0545

    next-commit

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    // Establish the connection using the URI from the environment variable
-    await mongoose.connect(process.env.MONGODB_URI);
-
-    mongoose.connection.on("connected", () => {
-      console.log("Database connected");
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "weaveworks", 
     });
+
+    console.log("Database connected");
   } catch (error) {
     console.error("Database connection failed:", error.message);
     process.exit(1);
